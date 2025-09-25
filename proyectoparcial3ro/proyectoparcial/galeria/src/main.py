af403fea752e350e598f1f8401532a3da41aecb2
corderoplatadesireeodette-blip <corderoplatadesireeodette@cetis50cdmx.com>
2025-09-24 20:44:38 -0600

    Update main.py

--- proyectoparcial3ro/proyectoparcial/galeria/src/main.py
+++ proyectoparcial3ro/proyectoparcial/galeria/src/main.py
@@ -8,9 +8,21 @@ page.bgcolor=ft.ColorsBLACK45
 
 perdonajes=[
   {
+   "titulo":"Katsuki Bakugo",
+   "autor":"Korei Horikoshi",
+   "edad":16-17,
+   "descripcion":"Orgulloso,explosivo y desidido,estudiante de heroes con gran talento.",
+                                      "imagen":"https://raw.githubusercontent.com/corderoplatadesireeodette-blip/proyecto-bimestral-1er-periodo/refs/heads/main/bakugo%20katsuki.avif"
+  }
+  {
+  "titulo":"Ben Drowned",
+   "autor":"Alexander D. Hall",
+   "edad":12,
+   "descripcion":."es una entidad sobrenatural que habita en cartucho maldito del videojuego tHE Legend of Zelda",
     
     page.add()
 
 
 ft.app(main)
+
 
